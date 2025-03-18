79e0f674b20b9509d4726f07600c37d7f74bf755
Prutha-Upadhyay <pruthu.ad@gmail.com>
2025-03-18 22:45:13 +0530

    Changes in config file

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -3,42 +3,17 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCCIY6v-7pHYSJiZ-TC93cUXqs7rCuT8OU",
-    authDomain: "attendance-system-8d55e.firebaseapp.com",
-    projectId: "attendance-system-8d55e",
-    storageBucket: "attendance-system-8d55e.firebasestorage.app",
-    messagingSenderId: "185209952950",
-    appId: "1:185209952950:web:57f2405a80b2bee30d6417",
-    measurementId: "G-N63WHC25XV"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
 export { app, auth, db };
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCCIY6v-7pHYSJiZ-TC93cUXqs7rCuT8OU",
-//   authDomain: "attendance-system-8d55e.firebaseapp.com",
-//   projectId: "attendance-system-8d55e",
-//   storageBucket: "attendance-system-8d55e.firebasestorage.app",
-//   messagingSenderId: "185209952950",
-//   appId: "1:185209952950:web:57f2405a80b2bee30d6417",
-//   measurementId: "G-N63WHC25XV"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
-
-// project-185209952950
