1ab11b0e6392c0e384516d9b539e4e47aab9916d
Prutha-Upadhyay <pruthu.ad@gmail.com>
2025-03-21 15:43:31 +0530

    git ignore changes

--- backend/firebaseConfig.js
+++ backend/firebaseConfig.js
@@ -1,12 +1,12 @@
 import admin from "firebase-admin";
 import dotenv from "dotenv";
-import firebaseServiceAccount from "./config/firebaseServiceAccount.json" assert { type: "json" };
+// import firebaseServiceAccount from "./config/firebaseServiceAccount.json" assert { type: "json" };
 
 
 dotenv.config();
-
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 admin.initializeApp({
-    credential: admin.credential.cert(firebaseServiceAccount),
+    credential: admin.credential.cert(serviceAccount),
     databaseURL: process.env.FIREBASE_DATABASE_URL,
 });
 
